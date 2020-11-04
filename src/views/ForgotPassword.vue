4dec7eacfb9293857d54f2586108ca56e3c38016
Hamdan <hamdan230598@gmail.com>
2020-11-04 21:06:35 +0700

    modified ForgotPassword

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -13,8 +13,66 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+// var nodemailer = require('nodemailer')
+// import nodemailer from 'nodemailer'
+// var fs = require('fs')
 export default {
-  name: 'Forgot Password'
+  name: 'Forgot Password',
+  data () {
+    return {
+      email: ''
+    }
+  },
+  methods: {
+    ...mapActions(['forgotPassword']),
+    handleRegister (e) {
+      e.preventDefault()
+      // var transporter = nodemailer.createTransport({
+      //   service: 'gmail',
+      //   auth: {
+      //     user: 'mychat709@gmail.com',
+      //     pass: 'Konsisten1!'
+      //   }
+      // })
+      alert(this.email)
+      const data = {
+        email: this.email
+      }
+      this.forgotPassword(data)
+        .then(res => {
+          alert('masuk')
+
+          // var transporter = nodemailer.createTransport({
+          //   service: 'gmail',
+          //   auth: {
+          //     user: 'mychat709@gmail.com',
+          //     pass: 'Konsisten1!'
+          //   }
+          // })
+
+          // var template = fs.readFileSync('index.html', 'utf-8')
+
+          // var mailOptions = {
+          //   from: 'mychat709',
+          //   to: this.email,
+          //   subject: 'Ubah kata sandi',
+          //   // html: template
+          //   html: '<h1> Silahkan klik link di bawah</h1> <p>Silahkan klik tautan dibawah untuk mengubah kata sandi</p> <a href="http://localhost:8080/changepassword">Klik</a>'
+          // }
+          // console.log(mailOptions.html)
+
+          // transporter.sendMail(mailOptions, (err, info) => {
+          //   if (err) throw err
+          //   console.log('Email sent: ' + info.response)
+          // })
+        })
+        .catch(err => {
+          alert('err')
+          console.log(err)
+        })
+    }
+  }
 }
 </script>
 
